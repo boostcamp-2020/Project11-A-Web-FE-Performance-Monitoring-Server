743ccc7fa3bddf7954017425dd00d6be9e9769bd
mononomo <hostdog01@naver.com>
2020-11-17 21:38:17 +0900

    chore : prettierrc 적용 완료 preetier 까지 모두 적용 완료했습니다. #2

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from "express";
 class App {
-  public application : express.Application;
-  constructor(){
+  public application: express.Application;
+  constructor() {
     this.application = express();
   }
-  test(params:string): string {
+  test(params: string): string {
     return params + " world !";
   }
 }
-const app = new App();
 
+const app = new App();
 console.log(app.test("hello"));
-app.application.get("/",(req : express.Request , res : express.Response) =>{
+app.application.get("/", (req: express.Request, res: express.Response) => {
   res.send("start");
-})
-app.application.listen(4000,()=>console.log("start"));
+});
+app.application.listen(4000, () => console.log("start"));
