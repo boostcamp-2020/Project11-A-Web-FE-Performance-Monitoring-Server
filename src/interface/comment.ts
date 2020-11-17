ef671aad579b6f76691c7b452438cfa123d53da0
mononomo <hostdog01@naver.com>
2020-11-17 22:11:28 +0900

    feature : interface comment 생성 comment Interface를 만들었습니다. #3

---
+++ src/interface/comment.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+  id: number;
+  userId: number;
+  description: string;
+  timeStamp: Date;
+}
--- src/interface/issue.ts
+++ src/interface/issue.ts
@@ -1,3 +1,5 @@
+import { Comment } from "./comment";
+
 export interface issue {
   id: number;
   devMode: string;
