d11b819f8319505b762d37f4c642be0f13e231b0
seokminKang <soos0228@naver.com>
2020-11-23 19:19:26 +0900

    feat: loaders 폴더 분리

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-class App {
-  public application: express.Application;
-  constructor() {
-    this.application = express();
-  }
-  test(params: string): string {
-    return params + " world !";
-  }
+import config from '@config/server';
+
+import express from 'express';
+
+import Loader from '@loaders';
+
+async function startServer() {
+  const app = express();
+
+  await Loader({ app });
+
+  app.listen(config.port, () => {
+    console.log(`Example app listening at http://localhost:${config.port}`);
+  });
 }
 
-const app = new App();
-console.log(app.test("hello"));
-app.application.get("/", (req: express.Request, res: express.Response) => {
-  res.send("start");
-});
-app.application.listen(4000, () => console.log("start"));
+startServer();
