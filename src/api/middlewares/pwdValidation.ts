723a60ba419b7df241646932dd0ab37e8034ef5f
seokminkkk <soos0228@naver.com>
2020-11-24 23:25:41 +0900

    feat: 패스워드 유효성 검사 미들웨어 추가

---
+++ src/api/middlewares/pwdValidation.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+
+// 최소 8 자, 대문자 하나 이상, 소문자 하나, 숫자 하나 및 특수 문자 하나 이상
+const pwdPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
+
+const pwdValidation = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  if (!pwdPattern.test(req.body.pwd)) {
+    throw new Error('비밀번호 규칙이 다릅니다.');
+  }
+  next();
+};
+
+export default pwdValidation;
