28402f6baff030235970da3672217b84b893e8ee
seokminkkk <soos0228@naver.com>
2020-11-24 23:20:58 +0900

    feat: mongoose DB 모델 추가

--- src/models/issue.ts
+++ src/models/issue.ts
@@ -1,10 +1,28 @@
 import { Issue as IIssue } from '@interfaces/issue';
 import mongoose from 'mongoose';
 
-const Issue = new mongoose.Schema({
-  devMode: String,
-  projectId: Number,
-  isResolved: Boolean,
-});
+const Issue = new mongoose.Schema(
+  {
+    devMode: { type: String, required: true },
+
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Project',
+      required: true,
+    },
+
+    error: {
+      type: String,
+      required: true,
+    },
+
+    isResolved: { type: Boolean, required: true },
+
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+
+    events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model<IIssue & mongoose.Document>('Issue', Issue);
