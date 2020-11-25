26409f6c7929a5e070f3cc3d7f66b9467d0a74ff
Hera <wnaltnr95@naver.com>
2020-11-25 10:48:37 +0900

    Update controller.ts
    이렇게 바꿔주세용

--- src/api/route/admin/auth/controller.ts
+++ src/api/route/admin/auth/controller.ts
@@ -3,12 +3,8 @@ import authService from '@services/auth';
 
 const join = async (req: Request, res: Response): Promise<void> => {
   const userInfo = req.body;
-  try {
-    await authService.join(userInfo);
-    return res.status(201).end();
-  } catch (err) {
-    throw new Error(err);
-  }
+  await authService.join(userInfo);
+  return res.status(201).end();
 };
 
 const login = (req: Request, res: Response): Response<void> => {
