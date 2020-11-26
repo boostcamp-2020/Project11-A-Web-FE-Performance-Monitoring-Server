6096cbf07931ed1cbd5fcca70a418b251d002669
seokminkkk <soos0228@naver.com>
2020-11-26 19:32:07 +0900

    refactor: 이벤트 타입 SDK 와 호환되도록 설정

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,27 +1,31 @@
 import { Event as IEvent } from '@interfaces/event';
 import mongoose from 'mongoose';
 
-const Event = new mongoose.Schema(
-  {
-    issueId: { type: mongoose.Schema.Types.ObjectId, ref: 'Issue' },
-    createdBy: {
-      ip: String,
-      email: String,
-      userId: String,
-    },
-    sdk: {
-      version: String,
-      name: String,
-    },
-    os: {
-      version: String,
-      name: String,
-    },
-    browser: {
-      version: String,
-      name: String,
-    },
+const Event = new mongoose.Schema({
+  issueId: { type: mongoose.Schema.Types.ObjectId, ref: 'Issue' },
+  createdBy: {
+    ip: String,
+    email: String,
+    userId: String,
   },
-  { timestamps: true },
-);
+  sdk: {
+    version: String,
+    name: String,
+  },
+  os: {
+    version: String,
+    name: String,
+  },
+  browser: {
+    version: String,
+    name: String,
+  },
+  timeStamp: Date,
+  url: String,
+  type: String,
+  value: String,
+  stactTrace: [Object],
+  platform: String,
+  context: [String],
+});
 export default mongoose.model<IEvent & mongoose.Document>('Event', Event);
