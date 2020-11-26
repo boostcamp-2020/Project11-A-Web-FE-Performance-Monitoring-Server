5a11ddb14f8d8aada48ca594d8beb5f00df29b00
seokminkkk <soos0228@naver.com>
2020-11-26 16:54:41 +0900

    fix: 오류메세지가 안오던 현상 수정

--- src/loaders/passport/local.ts
+++ src/loaders/passport/local.ts
@@ -1,6 +1,8 @@
-import { Strategy } from 'passport-local';
+import { Strategy, IVerifyOptions } from 'passport-local';
 import jwt from 'jsonwebtoken';
 
+import { User } from '@interfaces/user';
+import { Document } from 'mongoose';
 import passportConfig from '@config/passport';
 import bcrypt from '@root/utils/passwordBcrypt';
 import db from '@models';
@@ -11,17 +13,25 @@ const localConfig = {
   session: false,
 };
 
-const localFunction = async (email: string, pwd: string, done: any) => {
+const localFunction = async (
+  email: string,
+  pwd: string,
+  done: (
+    error: Error | null,
+    user?: (User & Document) | false,
+    options?: IVerifyOptions | undefined,
+  ) => void,
+) => {
   const user = await db.User.findOne({ email });
   if (!user) {
-    return done(null, false, { message: '가입되지 않은 이메일입니다.' });
+    return done(new Error('가입되지 않은 이메일입니다.'), false);
   }
   const isCorrectPassword = await bcrypt.checkPw(pwd, user.pwd);
   if (!isCorrectPassword) {
-    return done(null, false, { message: '비밀번호가 다릅니다.' });
+    return done(new Error('비밀번호가 다릅니다.'), false);
   }
   const token = jwt.sign(user.toJSON(), passportConfig.secretOrKey);
-  return done(null, { user, token });
+  return done(null, { ...user.toJSON(), token });
 };
 
 export default new Strategy(localConfig, localFunction);
