98c2c7347e1f15edeb0269b79ab9c36c82833e4d
seokminkkk <soos0228@naver.com>
2020-11-26 19:46:48 +0900

    feat: SDK 미들웨어 추가 - ProjectId 기반

---
+++ src/api/middlewares/sdkMiddleware.ts
@@ -0,0 +1,28 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { SDKRequest } from '@interfaces/express/sdkRequest';
+import tokenConfig from '@config/passport';
+import db from '@models';
+
+const sdkTokenVerify = async (
+  req: SDKRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  if (!req.headers.authorization) {
+    return res.status(401).end();
+  }
+  const token: string[] = req.headers.authorization.split(' ');
+  if (token[0] !== 'Bearer') {
+    return res.status(400).end();
+  }
+  const projectId = jwt.verify(token[1], tokenConfig.secretOrKey);
+  const project = await db.Project.findById(projectId);
+  if (!project) {
+    return next(new Error('올바르지 않은 토큰입니다.'));
+  }
+  req.project = project;
+  return next();
+};
+
+export default sdkTokenVerify;
