31854261eaaff09c269be9808ef6d2791881bf90
seokminkkk <soos0228@naver.com>
2020-11-26 16:50:14 +0900

    feat: checkEmail 기능 추가

--- src/api/route/admin/auth/controller.ts
+++ src/api/route/admin/auth/controller.ts
@@ -1,20 +1,41 @@
-import { Response, Request } from 'express';
-import authService from '@services/auth';
+import { Response, Request, NextFunction } from 'express';
+import authService from '@root/services/auth/auth';
+import checkEmailService from '@services/auth/checkEmail';
 
-const join = async (req: Request, res: Response): Promise<void> => {
+const join = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   const userInfo = req.body;
   try {
     await authService.join(userInfo);
-    return res.status(201).end();
+    return res.status(200).end();
   } catch (err) {
-    throw new Error(err);
+    next(new Error(err));
   }
 };
 
 const login = (req: Request, res: Response): Response<void> => {
   return res.json({
-    user: req.user,
+    ...req.user,
   });
 };
 
-export default { join, login };
+const checkEmail = async (
+  req: Request,
+  res: Response,
+): Promise<void | Response<void>> => {
+  try {
+    const checkResult = await checkEmailService.checkEmail(req.body.email);
+    if (checkResult) {
+      return res.status(200).end();
+    } else {
+      return res.status(400).json({ message: '이미 존재하는 이메일 입니다.' });
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export default { join, login, checkEmail };
--- src/api/route/admin/index.ts
+++ src/api/route/admin/index.ts
@@ -1,8 +1,12 @@
 import authRouter from './auth';
+import issueRouter from './issue';
+import projectRouter from './project';
 import { Router } from 'express';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/issue', issueRouter);
+router.use('/project', projectRouter);
 
 export default router;
