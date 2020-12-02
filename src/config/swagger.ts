a1eadaf34e7b437d79d169e1e07462e6a8c0c0cb
seokmin <soos0228@naver.com>
2020-12-02 21:34:02 +0900

    feat: swagger 세팅 변경

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -20,7 +20,7 @@ const options = {
   swaggerDefinition: swaggerDefinition,
   // Path to the API docs
   // apis: ['./**/swagger.ts'],
-  apis: ['./src/api/route/**/*.yaml'],
+  apis: ['./src/swagger/**/*.yaml'],
 };
 
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
