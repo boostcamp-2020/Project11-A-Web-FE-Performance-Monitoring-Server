55e7293d84cbc303e8a979b7289daed999db670a
seokmin <soos0228@naver.com>
2020-12-02 22:32:45 +0900

    fix: webpack 설정 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,6 @@ module.exports = {
       wait_ready: true, // Node.js 앱으로부터 앱이 실행되었다는 신호를 직접 받겠다는 의미
       listen_timeout: 50000, // 앱 실행 신호까지 기다릴 최대 시간. ms 단위.
       kill_timeout: 5000, // 새로운 프로세스 실행이 완료된 후 예전 프로세스를 교체하기까지 기다릴 시간
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: '3000',
-      },
     },
   ],
 };
