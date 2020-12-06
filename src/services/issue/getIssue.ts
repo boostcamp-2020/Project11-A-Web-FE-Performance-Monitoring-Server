c0ac79f8539c5e2a6010d996b6614e2b16f28e26
seokmin <soos0228@naver.com>
2020-12-06 21:13:29 +0900

    refactor: Error: Error: 로 설정된 현상 수정

--- src/services/issue/getIssue.ts
+++ src/services/issue/getIssue.ts
@@ -13,14 +13,14 @@ const getIssue = async (_id: string, issueId: string): Promise<Issue> => {
       })
       .exec();
     if (!targetIssue) {
-      throw new Error('찾는 이슈가 없습니다.');
+      throw '찾는 이슈가 없습니다.';
     }
     const targetProject = await db.Project.findOne({
       _id: targetIssue.projectId,
       $or: [{ owner: _id }, { admins: _id }, { members: _id }],
     }).exec();
     if (!targetProject) {
-      throw new Error('당신의 프로젝트가 아닙니다.');
+      throw '당신의 프로젝트가 아닙니다.';
     }
     return targetIssue;
   } catch (err) {
