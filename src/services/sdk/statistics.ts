374f82f0db18e0b3a61fd48a56213932c7c19cc2
seokmin <soos0228@naver.com>
2020-12-09 19:11:24 +0900

    refactor: type ? 삭제

--- src/services/sdk/statistics.ts
+++ src/services/sdk/statistics.ts
@@ -20,15 +20,15 @@ const addStatistics = async (issueId: string, event: Event): Promise<void> => {
   const tempDB = db.Statistics.findOne({ issueId }).exec();
   const tempStatistic: preStatistics = {};
   if (event.browser) {
-    tempStatistic.browser = event.browser?.name + ' ' + event.browser.version;
+    tempStatistic.browser = event.browser.name + ' ' + event.browser.version;
     delete event.browser;
   }
   if (event.sdk) {
-    tempStatistic.sdk = event.sdk?.name + ' ' + event.sdk.version;
+    tempStatistic.sdk = event.sdk.name + ' ' + event.sdk.version;
     delete event.sdk;
   }
   if (event.os) {
-    tempStatistic.os = event.os?.name + ' ' + event.os.version;
+    tempStatistic.os = event.os.name + ' ' + event.os.version;
     delete event.os;
   }
   const inputStatistic = { ...event, ...tempStatistic } as preStatistics;
