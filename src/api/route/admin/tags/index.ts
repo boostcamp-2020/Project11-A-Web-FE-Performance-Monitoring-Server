141de8f55bc060a47566cf03654f49f84b89ae43
seokmin <soos0228@naver.com>
2020-12-09 19:09:53 +0900

    feat: tag API 추가

---
+++ src/api/route/admin/tags/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import Controller from './controller';
+import passportMiddleware from '@middlewares/passportJwt';
+
+const router = Router();
+
+router.use(passportMiddleware);
+
+router.get('/:issueId', Controller.getTags);
+
+export default router;
