f90039e93b970654a6360998fbd8fab1ec1a3faf
seokmin <soos0228@naver.com>
2020-12-11 01:49:08 +0900

    test: Login 데이터 생성

---
+++ test/models/users.ts
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    email: 'jesttest1@naver.com',
+    pwd: 'aaAA1234@',
+  },
+  {
+    email: 'jesttest2@navercom',
+    pwd: 'aaAA1234@',
+  },
+  {
+    email: 'jesttest3@naver.com',
+    pwd: 'aaAA1234',
+  },
+];
