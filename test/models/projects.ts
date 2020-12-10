f856c6be99f6310dbca5c825859343e1e95266f2
seokmin <soos0228@naver.com>
2020-12-11 01:48:53 +0900

    test: Project 데이터 생성

---
+++ test/models/projects.ts
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    platform: 'Node',
+    projectName: '노드 플랫폼 프로젝트',
+  },
+  {
+    platform: 'Express',
+    projectName: '익스프레스 플랫폼 프로젝트',
+    emails: ['soos0228@naver.com', 'soos3121@gmail.com'],
+  },
+  {
+    platform: 'Node',
+    projectName: '이메일 타입 에러',
+    emails: ['soos0228@navercom', 'soos3121@gmail.com'],
+  },
+];
