92c3e01d043eaf05e89192440931a759f60521a4
seokmin <soos0228@naver.com>
2020-12-12 17:27:05 +0900

    feat: login 로직 구현

---
+++ src/services/auth/login.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+import { User } from '@interfaces/models/user';
+import passportConfig from '@config/passport';
+import bcrypt from '@root/utils/passwordBcrypt';
+import db from '@models';
+
+interface UserWithToken extends User {
+  token: string;
+}
+
+const login = async (email: string, pwd: string): Promise<UserWithToken> => {
+  const user = await db.User.findOne({ email });
+  if (!user) {
+    throw '가입되지 않은 이메일입니다.';
+  }
+  const isCorrectPasswordPromise = bcrypt.checkPw(pwd, user.pwd);
+  const token = jwt.sign(user.toJSON(), passportConfig.secretOrKey);
+  const isCorrectPassword = await isCorrectPasswordPromise;
+  if (!isCorrectPassword) {
+    throw '비밀번호가 다릅니다.';
+  }
+  return { ...user.toJSON(), token };
+};
+
+export default login;
